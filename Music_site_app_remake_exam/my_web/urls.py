534eca2b271ee7c19c05a26a6f477717bb4df82f
Georgi St. Lukanov <goshiii87@gmail.com>
2023-06-20 11:39:16 +0200

    views without logic

--- Music_site_app_remake_exam/my_web/urls.py
+++ Music_site_app_remake_exam/my_web/urls.py
@@ -1,3 +1,33 @@
-urlpatterns = (
+from django.urls import path, include
 
+from Music_site_app_remake_exam.my_web.views import index, profile_details, profile_delete, album_add, album_details, \
+    album_edit, album_delete, profile_create
+
+urlpatterns = (
+    path('', index, name='index'),
+
+    path('profile/', include([
+        path('details/', profile_details, name='profile-details'),
+        path('delete/', profile_delete, name='profile-delete'),
+        path('create/', profile_create, name='profile-create'),
+    ])),
+
+    path('album/', include([
+        path('add/', album_add, name='album-add'),
+        path('details/<int:pk>/', album_details, name='album-details'),
+        path('edit/<int:pk>/', album_edit, name='album-edit'),
+        path('delete/<int:pk>/', album_delete, name='album-delete'),
+    ]))
 )
+
+"""
+http://localhost:8000/ - home page
+
+http://localhost:8000/album/add/ - add album page
+http://localhost:8000/album/details/<id>/ - album details page
+http://localhost:8000/album/edit/<id>/ - edit album page
+http://localhost:8000/album/delete/<id>/ - delete album page
+
+http://localhost:8000/profile/details/ - profile details page
+http://localhost:8000/profile/delete/ - delete profile page
+"""
--- Music_site_app_remake_exam/my_web/views.py
+++ Music_site_app_remake_exam/my_web/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def index(request):
+    profile = None
+    if profile is None:
+        return redirect('profile-create')
+
+    return render(request, 'index.html')
+
+
+def profile_create(request):
+    return render(request, 'profile/profile-create.html')
+
+
+def profile_details(request):
+    return render(request, '/')
+
+
+def profile_delete(request):
+    return render(request, '/')
+
+
+def album_add(request):
+    return render(request, '/')
+
+
+def album_details(request):
+    return render(request, '/')
+
+
+def album_edit(request):
+    return render(request, '/')
+
+
+def album_delete(request):
+    return render(request, '/')
